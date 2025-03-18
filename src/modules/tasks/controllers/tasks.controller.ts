44a512237f8421ebdade6ab62279b55592ed6f76
Gabriel <devpalhares@gmail.com>
2025-03-18 18:03:15 -0300

    feat: creating post/tasks and patch/tasks

--- src/modules/tasks/controllers/tasks.controller.ts
+++ src/modules/tasks/controllers/tasks.controller.ts
@@ -1,20 +1,34 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Patch,
+  Body,
+  Param,
+  BadRequestException,
+} from '@nestjs/common';
 import { TasksService } from '../services/tasks.service';
 import { CreateTaskDto } from '../dto/create-task.dto';
+import { UpdateTaskDto } from '../dto/update-task.dto';
 
 @Controller('tasks')
 class TasksController {
   constructor(private readonly tasksService: TasksService) {}
+
+  private validateUUID(id: string) {
+    if (!id.match(/^[0-9a-fA-F-]{36}$/)) {
+      throw new BadRequestException('Invalid UUID format');
+    }
+  }
 
   @Post()
   create(@Body() task: CreateTaskDto) {
     return this.tasksService.create(task);
   }
 
-  @Get()
-  findAll() {
-    return this.tasksService.findAll();
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+    this.validateUUID(id);
+    return this.tasksService.update(id, updateTaskDto);
   }
 }
-
 export { TasksController };
--- src/modules/tasks/services/tasks.service.ts
+++ src/modules/tasks/services/tasks.service.ts
@@ -3,24 +3,23 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { TaskEntity } from '../entities/task.entity';
 import { CreateTaskDto } from '../dto/create-task.dto';
+import { UpdateTaskDto } from '../dto/update-task.dto';
 
 @Injectable()
 class TasksService {
-  private tasksRepository: Repository<TaskEntity>;
-
   constructor(
-    @InjectRepository(TaskEntity) tasksRepository: Repository<TaskEntity>,
-  ) {
-    this.tasksRepository = tasksRepository;
-  }
+    @InjectRepository(TaskEntity)
+    private readonly tasksRepository: Repository<TaskEntity>,
+  ) {}
 
   async create(dto: CreateTaskDto) {
     const newTask = this.tasksRepository.create(dto);
     return this.tasksRepository.save(newTask);
   }
 
-  async findAll() {
-    return this.tasksRepository.find();
+  async update(id: string, dto: UpdateTaskDto) {
+    await this.tasksRepository.update(id, dto);
+    return this.tasksRepository.findOne({ where: { id } });
   }
 }
 
