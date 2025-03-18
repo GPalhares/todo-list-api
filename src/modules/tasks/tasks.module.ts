5954356bba74aae3f1175bc2203f7b83f98aae43
Gabriel <devpalhares@gmail.com>
2025-03-18 17:12:54 -0300

    fix: changed db password on app.module

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksService } from './services/tasks.service';
 import { TasksController } from './controllers/tasks.controller';
+import { TaskEntity } from './entities/task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TaskEntity])],
   providers: [TasksService],
   controllers: [TasksController],
 })
