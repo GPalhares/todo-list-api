13c9d5e1dba4c5594a41851bdc8cc67a2b0428e5
Gabriel <devpalhares@gmail.com>
2025-03-20 19:21:53 -0300

    fix: allowing deploy origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://todo-list-gw2s13fs4-gabriels-projects-c9ce35c6.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
